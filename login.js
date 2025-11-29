9ec266e169024d7aa27581a41060e4dcb725d94b
Jhnvncntx <jhnvncntx@gmail.com>
2025-11-29 19:41:10 +0800

    LOGIN PAGE

---
+++ login.js
@@ -0,0 +1,17 @@
+const pwd = document.getElementById('password');
+const toggle = document.getElementById('toggle-button');
+
+toggle.addEventListener('click', () => {
+  pwd.type = pwd.type === "password" ? "text" : "password";
+  if (pwd.type === "text") {
+    toggle.innerText = "Hide";
+  } else {
+    toggle.innerText = "Show";
+  }
+});
+
+if (pwd.type === "password") {
+    pwd.type = "text";
+} else {
+    pwd.type = "password";
+}
